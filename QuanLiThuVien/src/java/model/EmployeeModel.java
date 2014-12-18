27c33c744605fd369017253cec598fe0937caffe
tuyetphale993 <tuyetphale993@gmail.com>
2014-12-18 12:08:20 +0700

    Add some methods for ThemSach function
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

---
+++ QuanLiThuVien/src/java/model/EmployeeModel.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author SalmDo
+ */
+import entity.*;
+import java.util.*;
+import static javassist.CtMethod.ConstParameter.string;
+import javax.xml.bind.annotation.XmlElement;
+import org.hibernate.*;
+public class EmployeeModel {
+    
+    //    Add New Book
+    public List<Banphat> GetAllAuthors(){
+        return null;
+    }
+    public List<Nhaxuatban> GetAllPublisher(){
+        return null;
+    }
+    public List<Nhomsach> GetAllBookGroups(){
+        return null;
+    }
+    public List<Chude> GetAllTopics(){
+        return null;
+    }
+    public List<Vitri> GetAllFloors(){
+        return null;
+    }
+    public String SaveNewBook(XmlElement node)
+    {   return null;
+    }
+}
